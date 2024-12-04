18fa57e1aa24bfa8aa3c4b7eee1dc9e72f7f1ae4
Paola Maddaluno <83777310+PaolaDMadd-Pro@users.noreply.github.com>
2024-12-04 19:39:22 +0000

    EPP-32-add page title (TBC)
    - update fields/index.js
    - fields.json
    - pages.json

--- apps/epp/fields/index.js
+++ apps/epp/fields/index.js
@@ -1,8 +1,15 @@
 module.exports = {
-  name: {
+  'application-type': {
+    mixin: 'radio-group',
+    options: [{ value: 'yes' }, { value: 'no' }],
+  },
+  'your-name': {
     mixin: 'input-text',
     validate: ['required', 'notUrl'],
     labelClassName: 'govuk-label--s',
     className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'your-details': {
+    mixin: 'input-text',
   }
-};
+}
