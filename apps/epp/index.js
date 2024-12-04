7c73c8ccfd25657da40f5eae17704b0c669ac2dc
Paola Maddaluno <83777310+PaolaDMadd-Pro@users.noreply.github.com>
2024-12-04 15:54:43 +0000

    EPP-32 add initial flow (TBC)

--- apps/epp/index.js
+++ apps/epp/index.js
@@ -9,13 +9,51 @@ module.exports = {
   translations: 'apps/epp/translations',
   baseUrl: '/',
   steps: {
-    '/start': {
-      next: '/name'
+    '/': {
     },
-    '/name': {
+    '/application-type': {
       fields: ['name'],
-      next: '/confirm'
+      next: '/your-name'
     },
+    '/your-name': {
+      fields: ['name'],
+      //add fork for other-names yes
+      next: '/your-details'
+    },
+    '/other-names': {
+      fields: ['name'],
+      next: '/other-names-summary'
+    },
+    '/other-names-summary': {
+      fields: ['name'],
+      next: '/your-details'
+    },
+    '/your-details': {
+      fields: ['name'],
+     //add fork /other-nationalities
+     next:'/home-address'
+    },
+    '/other-nationalities':{
+      fields: ['name'],
+      next: '/home-address'
+    },
+    '/home-address': {
+      fields: ['name'],
+      //add fork /previous-address
+      next: '/upload-proof-address'
+    },
+    'previous-address':{
+      fields: ['name'],
+      //add fork /previous-address-summary
+      next: '/previous-address-summary'
+    },
+
+    '/previous-address-summary':{
+      fields: ['name'],
+      next: '/upload-proof-address'
+    },
+
+
     '/confirm': {
       behaviours: ['complete', summary],
       next: '/complete'
