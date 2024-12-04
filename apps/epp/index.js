7853b411c37f16b8a17233f9fdf9f3c02acd0864
Paola Maddaluno <83777310+PaolaDMadd-Pro@users.noreply.github.com>
2024-12-04 19:37:18 +0000

    EPP-32 complete initial flow and removed start page
    -add all pages for new flow to index.js

--- apps/epp/index.js
+++ apps/epp/index.js
@@ -9,14 +9,12 @@ module.exports = {
   translations: 'apps/epp/translations',
   baseUrl: '/',
   steps: {
-    '/': {
-    },
     '/application-type': {
-      fields: ['name'],
+      field:['application-type'],
       next: '/your-name'
     },
     '/your-name': {
-      fields: ['name'],
+      fields: ['your-name'],
       //add fork for other-names yes
       next: '/your-details'
     },
@@ -29,11 +27,11 @@ module.exports = {
       next: '/your-details'
     },
     '/your-details': {
-      fields: ['name'],
-     //add fork /other-nationalities
-     next:'/home-address'
+      fields: ['your-details'],
+      //add fork /other-nationalities
+      next: '/home-address'
     },
-    '/other-nationalities':{
+    '/other-nationalities': {
       fields: ['name'],
       next: '/home-address'
     },
@@ -42,21 +40,157 @@ module.exports = {
       //add fork /previous-address
       next: '/upload-proof-address'
     },
-    'previous-address':{
+    '/upload-proof-address': {
+      fields: ['name'],
+      next: '/contact-details'
+    },
+    'previous-address': {
       fields: ['name'],
       //add fork /previous-address-summary
       next: '/previous-address-summary'
     },
-
-    '/previous-address-summary':{
+    '/previous-address-summary': {
       fields: ['name'],
       next: '/upload-proof-address'
     },
-
-
+    '/contact-details': {
+      fields: ['name'],
+      next: '/identity-details'
+    },
+    '/identity-details': {
+      fields: ['name'],
+      // 3 option for next path
+      next: '/upload-british-passport'
+    },
+    '/upload-british-passport': {
+      fields: ['name'],
+      next: '/other-licences'
+    },
+    '/upload-driving-licence': {
+      fields: ['name'],
+      next: '/other-licences'
+    },
+    '/upload-passport': {
+      fields: ['name'],
+      next: '/upload-certificate-conduct'
+    },
+    '/upload-certificate-conduct': {
+      fields: ['name'],
+      next: '/other-licences'
+    },
+    '/other-licences': {
+      fields: ['name'],
+      //add fork /add-licence-refusal
+      next: '/criminal-record'
+    },
+    '/add-licence-refusal': {
+      fields: ['name'],
+      next: '/licence-history'
+    },
+    '/licence-history': {
+      fields: ['name'],
+      next: '/criminal-record'
+    },
+    '/criminal-record': {
+      fields: ['name'],
+      //add fork for /add-offence
+      next: '/add-offence'
+    },
+    '/add-offence': {
+      fields: ['name'],
+      next: '/criminal-record-summary'
+    },
+    '/criminal-record-summary': {
+      fields: ['name'],
+      next: '/medical-declaration'
+    },
+    '/medical-declaration': {
+      fields: ['name'],
+      next: '/medical-history'
+    },
+    '/medical-history': {
+      fields: ['name'],
+      //add fork for medical-form
+      next: '/doctor-details'
+    },
+    '/medical-form': {
+      fields: ['name'],
+      next: '/doctor-details'
+    },
+    '/doctor-details': {
+      fields: ['name'],
+      next: '/precursor-or-poison'
+    },
+    '/precursor-or-poison': {
+      fields: ['name'],
+      next: '/select-precursor'
+    },
+    '/select-precursor': {
+      fields: ['name'],
+      next: '/precursors-summary'
+    },
+    '/precursors-summary': {
+      fields: ['name'],
+      next: '/select-poison'
+    },
+    '/select-poison': {
+      fields: ['name'],
+      next: '/poison-details'
+    },
+    '/poison-details': {
+      fields: ['name'],
+      next: '/poison-summary'
+    },
+    '/poison-summary': {
+      fields: ['name'],
+      next: '/countersignatory-details'
+    },
+    '/countersignatory-details': {
+      fields: ['name'],
+      next: '/countersignatory-address'
+    },
+    '/countersignatory-address': {
+      fields: ['name'],
+      next: '/countersignatory-contact'
+    },
+    '/countersignatory-contact': {
+      fields: ['name'],
+      next: '/countersignatory-id'
+    },
+    '/countersignatory-id': {
+      fields: ['name'],
+      //add logic to check if user is 18 to redirect to birth certificate
+      next: '/confirm'
+    },
+    '/birth-certificate': {
+      fields: ['name'],
+      next: '/confirm'
+    },
     '/confirm': {
       behaviours: ['complete', summary],
-      next: '/complete'
+      next: '/declaration'
+    },
+    '/declaration': {
+      fields:['name'],
+      //verify path name when payment component will be added to service
+      next: '/continue-to-payment'
+    },
+    '/continue-to-payment':{
+      next: '/application-submitted'
+    },
+    '/payment-problem':{
+      fields:['name'],
+      next: '/continue-to-payment'
+    },
+    '/payment-failed':{
+      fields:['name'],
+      next: '/continue-to-payment'
+    },
+    '/payment-cancelled':{
+      fields:['name'],
+      next: '/continue-to-payment'
+    },
+    '/application-submitted': {
     },
     '/complete': {
       backLink: false
