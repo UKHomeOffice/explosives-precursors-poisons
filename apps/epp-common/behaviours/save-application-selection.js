501bb64e1161b55ed720b6c6f69ac5a178f7d265
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2024-12-12 16:46:28 +0000

    EPP-32 add service name

--- apps/epp-common/behaviours/save-application-selection.js
+++ apps/epp-common/behaviours/save-application-selection.js
@@ -4,7 +4,9 @@ module.exports = superclass =>
       const applicationType = req.form.values['application-type'];
       req.sessionModel.set('applicationType', applicationType);
       const isRenewJourney = applicationType === 'renew';
+      const isNewJourney = applicationType === 'new';
       req.sessionModel.set('isRenewJourney', isRenewJourney);
+      req.sessionModel.set('isNewJourney', isNewJourney);
       return super.saveValues(req, res, next);
     }
   };
