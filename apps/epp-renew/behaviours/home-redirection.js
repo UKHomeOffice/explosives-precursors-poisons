dd65a6b18baf4b197280f2027914ad948cc61a77
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2024-12-12 11:25:43 +0000

    EPP-32 protect license-number page access

--- apps/epp-renew/behaviours/home-redirection.js
+++ apps/epp-renew/behaviours/home-redirection.js
@@ -2,7 +2,7 @@ module.exports = superclass =>
   class extends superclass {
     getValues(req, res, next) {
       const selectedAppType = req.sessionModel.get('applicationType');
-      if (!selectedAppType) {
+      if (!selectedAppType || selectedAppType !== 'renew') {
         return res.redirect('/application-type');
       }
       return super.getValues(req, res, next);
