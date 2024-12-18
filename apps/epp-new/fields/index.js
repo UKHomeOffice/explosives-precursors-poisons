b3d7cee4de7ebd7a0502969098e496de69657cae
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2024-12-18 13:11:01 +0000

    EPP-39 other nationalities page

--- apps/epp-new/fields/index.js
+++ apps/epp-new/fields/index.js
@@ -1,1 +1,28 @@
-module.exports = {};
+const dateComponent = require('hof').components.date;
+const countries = require('hof').utils.countries();
+
+module.exports = {
+  'new-renew-other-country-nationality': {
+    mixin: 'select',
+    className: ['typeahead'],
+    labelClassName: 'bold',
+    options: [
+      {
+        value: '',
+        label:
+          'fields.new-renew-other-country-nationality.options.none_selected'
+      }
+    ].concat(countries),
+    validate: 'required'
+  },
+  'new-renew-from-date': dateComponent('new-renew-from-date', {
+    mixin: 'input-date',
+    legend: { className: 'bold' },
+    validate: ['date', { type: 'before', arguments: ['0', 'days'] }]
+  }),
+  'new-renew-to-date': dateComponent('new-renew-to-date', {
+    mixin: 'input-date',
+    legend: { className: 'bold' },
+    validate: ['date', { type: 'before', arguments: ['0', 'days'] }]
+  })
+};
