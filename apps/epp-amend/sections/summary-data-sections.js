7f788645f84f2f9675c3074bf91590616485eb34
Paola Maddaluno <83777310+PaolaDMadd-Pro@users.noreply.github.com>
2024-12-19 18:08:29 +0000

    EPP-57 amend licence path
    - add fields in fields/index.js, fields.json, page.json, validation.json
    - add step in summary-data-sections.js
    - defined steps in index.js

--- apps/epp-amend/sections/summary-data-sections.js
+++ apps/epp-amend/sections/summary-data-sections.js
@@ -1,1 +1,20 @@
+'use strict';
 
+module.exports = {
+  'Licence details': {
+    steps: [
+      {
+        step: '/amend-licence-number',
+        field: 'amend-licence-number'
+      }
+    ]
+  },
+  'Applicant name': {
+    steps: [
+      {
+        step: '/amend-name-on-licence',
+        fields: ['amend-name-title', 'amend-firstname', 'amend-middlename', 'amend-lastname']
+      }
+    ]
+  }
+};
