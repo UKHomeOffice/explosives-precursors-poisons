984f37d285a2cfea2938a4396b635ac59edf6a5d
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2025-01-03 19:07:40 +0000

    EPP 61 Licence number page
    - added code refactoring by adding a custom max and min length for licence number, also added this for amend licence number.
    
    - Added behavoiur in the confirm page to check application type for different content display, also added relvant values display in the confirm page.
    
    - Added content on the renew-licence page according to figma designs
    
    - Added error validation to check for validations rules.

--- apps/epp-new/behaviours/confirmation-type.js
+++ apps/epp-new/behaviours/confirmation-type.js
@@ -1,13 +1,5 @@
 module.exports = superclass =>
   class extends superclass {
-    getValues(req, res, next) {
-      const selectedAppType = req.sessionModel.get('applicationType');
-      if (!selectedAppType) {
-        return res.redirect('/application-type');
-      }
-      return super.getValues(req, res, next);
-    }
-
     locals(req, res) {
       const locals = super.locals(req, res);
       const applicationType = req.form.values['application-type'];
