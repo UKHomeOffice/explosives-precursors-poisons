f40633959c6ce9d958a7c3730e542854c7a85a51
Mike Maratheftis <160880141+MikeMaratheftis@users.noreply.github.com>
2025-01-09 10:17:06 +0000

    EPP-35 New & Renew - Create a YOUR NAME page (#19)
    * EPP-35 Create your name page
    
    ---------
    
    Co-authored-by: Mike Maratheftis <mike.maratheftis@digital.homeoffice.gov.uk>

--- apps/epp-new/fields/index.js
+++ apps/epp-new/fields/index.js
@@ -1,15 +1,58 @@
-module.exports = {
+const titles = require('../../../utilities/constants/titles');
 
+module.exports = {
+  'new-renew-title': {
+    mixin: 'select',
+    validate: 'required',
+    labelClassName: 'govuk-label--s',
+    className: ['govuk-select', 'govuk-input--width-2'],
+    options: [
+      {
+        value: '',
+        label: 'fields.new-renew-title.options.none_selected'
+      }
+    ].concat(titles)
+  },
+  'new-renew-first-name': {
+    mixin: 'input-text',
+    validate: ['required', 'notUrl'],
+    labelClassName: 'govuk-label--s',
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'new-renew-middle-name': {
+    mixin: 'input-text',
+    validate: ['notUrl', { type: 'maxlength', arguments: [250] }],
+    labelClassName: 'govuk-label--s',
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'new-renew-last-name': {
+    mixin: 'input-text',
+    validate: ['required', 'notUrl', { type: 'maxlength', arguments: [250] }],
+    labelClassName: 'govuk-label--s',
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'new-renew-other-names': {
+    mixin: 'radio-group',
+    options: [
+      { value: 'yes', label: 'Yes' },
+      { value: 'no', label: 'No' }
+    ],
+    validate: 'required',
+    className: ['govuk-radios', 'govuk-radios--inline'],
+    legend: {
+      className: 'govuk-fieldset__legend govuk-fieldset__legend--s'
+    }
+  },
   'new-renew-phone-number': {
     mixin: 'input-text',
+    validate: ['required', 'internationalPhoneNumber'],
     className: ['govuk-input'],
-    labelClassName: 'govuk-label--m',
-    validate: ['required', 'internationalPhoneNumber']
+    labelClassName: 'govuk-label--m'
   },
   'new-renew-email': {
     mixin: 'input-text',
+    validate: ['required', 'email'],
     className: ['govuk-input'],
-    labelClassName: 'govuk-label--m',
-    validate: ['required', 'email']
+    labelClassName: 'govuk-label--m'
   }
 };
--- apps/epp-new/sections/summary-data-sections.js
+++ apps/epp-new/sections/summary-data-sections.js
@@ -7,14 +7,38 @@ module.exports = {
       field: 'new-renew-license-number'
     }
   ],
+  'your-name': {
+    steps: [
+      {
+        step: '/your-name',
+        field: 'new-renew-title'
+      },
+      {
+        step: '/your-name',
+        field: 'new-renew-first-name'
+      },
+      {
+        step: '/your-name',
+        field: 'new-renew-middle-name'
+      },
+      {
+        step: '/your-name',
+        field: 'new-renew-last-name'
+      },
+      {
+        step: '/your-name',
+        field: 'new-renew-other-names'
+      }
+    ]
+  },
   'new-renew-contact-details': {
     steps: [
       {
-        steps: '/contact-details',
+        step: '/contact-details',
         field: 'new-renew-phone-number'
       },
       {
-        steps: '/contact-details',
+        step: '/contact-details',
         field: 'new-renew-email'
       }
     ]
