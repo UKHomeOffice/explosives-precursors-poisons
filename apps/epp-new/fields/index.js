bd8a61ca2ff708152d1c6e5a06bae137423acf76
Vivek Kumar <Vivek.Kumar1@digital.homeoffice.gov.uk>
2025-01-20 12:31:02 +0000

    EPP-38 New & Renew - Your details page (#32)
    * EPP-38 your details page initial commit
    
    * EPP-39 added validation error messages
    
    * EPP-38 resolved conflicts
    
    * EPP-38 added summary fields
    
    * EPP-38 added util for fullstop check
    
    * EPP-38 style changes for height field
    
    * EPP-38 use translation for text
    
    * EPP-38 confirm page changes
    
    * EPP-38 drop down default value updated
    
    * EPP-38 fixed label post merge conflicts
    
    * EPP-38 resolved conflicts
    
    * EPP-38 confirm page ordering fix

--- apps/epp-new/fields/index.js
+++ apps/epp-new/fields/index.js
@@ -1,6 +1,7 @@
 const dateComponent = require('hof').components.date;
 const titles = require('../../../utilities/constants/titles');
 const countries = require('../../../utilities/constants/countries');
+const { isWithoutFullStop } = require('../../../utilities/helpers');
 
 module.exports = {
   'new-renew-title': {
@@ -178,5 +179,99 @@ module.exports = {
     mixin: 'input-date',
     legend: { className: 'bold' },
     validate: ['date', 'before']
-  })
+  }),
+  'new-renew-dob': dateComponent('new-renew-dob', {
+    mixin: 'input-date',
+    legend: { className: 'bold' },
+    validate: ['required', 'date', 'before']
+  }),
+  'new-renew-birth-place': {
+    mixin: 'input-text',
+    labelClassName: 'bold',
+    validate: [
+      'required',
+      { type: 'minlength', arguments: 2 },
+      { type: 'maxlength', arguments: 250 },
+      'notUrl'
+    ]
+  },
+  'new-renew-birth-country': {
+    mixin: 'select',
+    className: ['typeahead'],
+    labelClassName: 'bold',
+    options: [
+      {
+        value: '',
+        label: 'fields.new-renew-birth-country.options.none_selected'
+      }
+    ].concat(countries),
+    validate: ['required']
+  },
+  'new-renew-country-nationality': {
+    mixin: 'select',
+    className: ['typeahead'],
+    labelClassName: 'bold',
+    options: [
+      {
+        value: '',
+        label: 'fields.new-renew-country-nationality.options.none_selected'
+      }
+    ].concat(countries),
+    validate: ['required']
+  },
+  'new-renew-more-nationalities': {
+    mixin: 'radio-group',
+    options: [
+      { value: 'yes', label: 'Yes' },
+      { value: 'no', label: 'No' }
+    ],
+    validate: 'required',
+    className: ['govuk-radios', 'govuk-radios--inline'],
+    legend: {
+      className: 'govuk-fieldset__legend govuk-fieldset__legend--s'
+    }
+  },
+  'new-renew-your-sex': {
+    mixin: 'radio-group',
+    options: [
+      { value: 'male', label: 'Male' },
+      { value: 'female', label: 'Female' },
+      { value: 'x', label: 'X or other' }
+    ],
+    validate: 'required',
+    className: ['govuk-radios'],
+    legend: {
+      className: 'govuk-fieldset__legend govuk-fieldset__legend--s'
+    }
+  },
+  'new-renew-your-height': {
+    mixin: 'input-text',
+    className: ['govuk-input', 'govuk-input--width-2'],
+    labelClassName: 'bold',
+    validate: [
+      'required',
+      'notUrl',
+      isWithoutFullStop,
+      'numeric',
+      {
+        type: 'min',
+        arguments: 50
+      },
+      {
+        type: 'max',
+        arguments: 300
+      }
+    ],
+    attributes: [{ suffix: 'cm' }]
+  },
+  'new-renew-occupation': {
+    mixin: 'input-text',
+    labelClassName: 'bold',
+    validate: [
+      'required',
+      { type: 'minlength', arguments: 2 },
+      { type: 'maxlength', arguments: 250 },
+      'notUrl'
+    ]
+  }
 };
--- apps/epp-new/index.js
+++ apps/epp-new/index.js
@@ -16,15 +16,18 @@ module.exports = {
   steps: {
     '/your-name': {
       behaviours: [checkBackLink, validateAndRedirect],
-      fields: ['new-renew-title',
+      fields: [
+        'new-renew-title',
         'new-renew-first-name',
         'new-renew-middle-name',
         'new-renew-last-name',
-        'new-renew-other-names'],
+        'new-renew-other-names'
+      ],
       forks: [
         {
           target: '/other-names',
-          condition: req => req.sessionModel.get('new-renew-other-names') === 'yes'
+          condition: req =>
+            req.sessionModel.get('new-renew-other-names') === 'yes'
         }
       ],
       next: '/your-details',
@@ -63,9 +66,26 @@ module.exports = {
       }
     },
     '/your-details': {
-      fields: ['new-renew-country-nationality'],
-      // add fork /other-nationalities
-      next: '/other-nationalities',
+      fields: [
+        'new-renew-dob',
+        'new-renew-birth-place',
+        'new-renew-birth-country',
+        'new-renew-country-nationality',
+        'new-renew-more-nationalities',
+        'new-renew-your-sex',
+        'new-renew-your-height',
+        'new-renew-occupation'
+      ],
+      forks: [
+        {
+          target: '/other-nationalities',
+          condition: {
+            field: 'new-renew-more-nationalities',
+            value: 'yes'
+          }
+        }
+      ],
+      next: '/home-address',
       locals: {
         sectionNo: {
           new: 2,
@@ -132,10 +152,7 @@ module.exports = {
       }
     },
     '/contact-details': {
-      fields: [
-        'new-renew-phone-number',
-        'new-renew-email'
-      ],
+      fields: ['new-renew-phone-number', 'new-renew-email'],
       next: '/identity-details',
       locals: {
         sectionNo: {
