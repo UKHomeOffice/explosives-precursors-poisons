9dab797571b53ff47d30ba2f5f6672624117995e
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2025-02-03 09:30:14 +0000

    EPP-48 updated document name

--- apps/epp-common/behaviours/remove-document.js
+++ apps/epp-common/behaviours/remove-document.js
@@ -1,13 +1,13 @@
 'use strict';
 
-module.exports = documentCategory => superclass =>
+module.exports = documentName => superclass =>
   class extends superclass {
     configure(req, res, next) {
       if (req.query.delete) {
-        const documents = req.sessionModel.get(documentCategory) || [];
+        const documents = req.sessionModel.get(documentName) || [];
         const remaining = documents.filter(d => d.id !== req.query.delete);
         req.log('info', `Removing document: ${req.query.delete}`);
-        req.sessionModel.set(documentCategory, remaining);
+        req.sessionModel.set(documentName, remaining);
         return res.redirect(`${req.baseUrl}${req.path}`);
       }
       return super.configure(req, res, next);
