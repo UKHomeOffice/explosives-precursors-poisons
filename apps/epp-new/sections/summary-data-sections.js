4335f0f433502a8aae00b734b945b2404b3836b3
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2025-02-05 15:49:48 +0000

    EPP-48 resolved conflicts

--- apps/epp-new/sections/summary-data-sections.js
+++ apps/epp-new/sections/summary-data-sections.js
@@ -222,22 +222,22 @@ module.exports = {
         step: '/identity-details',
         field: 'new-renew-Uk-driving-licence-number'
       },
-    {
+      {
         step: '/upload-british-passport',
         field: 'new-renew-british-passport',
         parse: (documents, req) => {
-            if (
-                req.sessionModel
-                    .get('steps')
-                    .includes('/upload-british-passport') &&
-                documents?.length > 0
-            ) {
-                return documents.map(file => file.name);
-            }
+          if (
+            req.sessionModel
+              .get('steps')
+              .includes('/upload-british-passport') &&
+            documents?.length > 0
+          ) {
+            return documents.map(file => file.name);
+          }
 
-            return null;
+          return null;
         }
-    }
+      }
     ]
   },
   'medical-information': {
