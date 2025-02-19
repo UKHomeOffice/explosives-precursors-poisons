f8219bac3b9b8fa9106bea192cdd38f2b632f4c1
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2025-02-19 09:10:08 +0000

    EPP-160 resolved merge conflicts

--- apps/epp-amend/fields/index.js
+++ apps/epp-amend/fields/index.js
@@ -317,80 +317,80 @@ module.exports = {
     options: ['yes', 'no'],
     validate: 'required'
   },
-    'amend-why-need-precursor': {
-        mixin: 'textarea',
-        validate: ['required', 'notUrl', helpers.textAreaDefaultLength],
-        attributes: [{ attribute: 'rows', value: 5 }],
-        labelClassName: 'govuk-label--s'
+  'amend-why-need-precursor': {
+    mixin: 'textarea',
+    validate: ['required', 'notUrl', helpers.textAreaDefaultLength],
+    attributes: [{ attribute: 'rows', value: 5 }],
+    labelClassName: 'govuk-label--s'
+  },
+  'amend-how-much-precursor': {
+    mixin: 'input-text',
+    validate: ['required', 'notUrl', { type: 'maxlength', arguments: 250 }],
+    className: ['govuk-input', 'govuk-input--width-10'],
+    labelClassName: 'govuk-label--s'
+  },
+  'amend-what-concentration-precursor': {
+    mixin: 'input-text',
+    validate: [
+      'required',
+      helpers.isValidConcentrationValue,
+      { type: 'maxlength', arguments: 250 },
+      'notUrl'
+    ],
+    className: ['govuk-input', 'govuk-input--width-5'],
+    labelClassName: 'govuk-label--s',
+    attributes: [{ suffix: '%' }]
+  },
+  'amend-where-to-store-precursor': {
+    mixin: 'checkbox-group',
+    validate: ['required'],
+    legend: {
+      className: 'govuk-label--s'
     },
-    'amend-how-much-precursor': {
-        mixin: 'input-text',
-        validate: ['required', 'notUrl', { type: 'maxlength', arguments: 250 }],
-        className: ['govuk-input', 'govuk-input--width-10'],
-        labelClassName: 'govuk-label--s'
+    options: [
+      {
+        value: 'amend-store-precursors-home-address'
+      },
+      {
+        value: 'amend-store-precursors-other-address',
+        toggle: 'store-precursors-other-address',
+        child: 'textarea'
+      }
+    ]
+  },
+  'store-precursors-other-address': {
+    mixin: 'textarea',
+    validate: ['required', helpers.textAreaDefaultLength, 'notUrl'],
+    dependent: {
+      field: 'amend-where-to-store-precursor',
+      value: 'amend-store-precursors-other-address'
+    }
+  },
+  'amend-where-to-use-precursor': {
+    mixin: 'checkbox-group',
+    validate: ['required'],
+    legend: {
+      className: 'govuk-label--s'
     },
-    'amend-what-concentration-precursor': {
-        mixin: 'input-text',
-        validate: [
-            'required',
-            helpers.isValidConcentrationValue,
-            { type: 'maxlength', arguments: 250 },
-            'notUrl'
-        ],
-        className: ['govuk-input', 'govuk-input--width-5'],
-        labelClassName: 'govuk-label--s',
-        attributes: [{ suffix: '%' }]
-    },
-    'amend-where-to-store-precursor': {
-        mixin: 'checkbox-group',
-        validate: ['required'],
-        legend: {
-            className: 'govuk-label--s'
-        },
-        options: [
-            {
-                value: 'amend-store-precursors-home-address'
-            },
-            {
-                value: 'amend-store-precursors-other-address',
-                toggle: 'store-precursors-other-address',
-                child: 'textarea'
-            }
-        ]
-    },
-    'store-precursors-other-address': {
-        mixin: 'textarea',
-        validate: ['required', helpers.textAreaDefaultLength, 'notUrl'],
-        dependent: {
-            field: 'amend-where-to-store-precursor',
-            value: 'amend-store-precursors-other-address'
-        }
-    },
-    'amend-where-to-use-precursor': {
-        mixin: 'checkbox-group',
-        validate: ['required'],
-        legend: {
-            className: 'govuk-label--s'
-        },
-        options: [
-            {
-                value: 'amend-use-precursors-home-address'
-            },
-            {
-                value: 'amend-use-precursors-other-address',
-                toggle: 'precursors-use-other-address',
-                child: 'textarea'
-            }
-        ]
-    },
-    'precursors-use-other-address': {
-        mixin: 'textarea',
-        validate: ['required', helpers.textAreaDefaultLength, 'notUrl'],
-        dependent: {
-            field: 'amend-where-to-use-precursor',
-            value: 'amend-use-precursors-other-address'
-        }
-    },
+    options: [
+      {
+        value: 'amend-use-precursors-home-address'
+      },
+      {
+        value: 'amend-use-precursors-other-address',
+        toggle: 'precursors-use-other-address',
+        child: 'textarea'
+      }
+    ]
+  },
+  'precursors-use-other-address': {
+    mixin: 'textarea',
+    validate: ['required', helpers.textAreaDefaultLength, 'notUrl'],
+    dependent: {
+      field: 'amend-where-to-use-precursor',
+      value: 'amend-use-precursors-other-address'
+    }
+  },
   'amend-countersignatory-title': {
     mixin: 'select',
     validate: 'required',
