7e2a6c5e200a4a89f2c9d75569bc5de391237cce
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2025-02-25 15:48:06 +0000

    EPP-160 updated poison short labels

--- utilities/constants/poisons.js
+++ utilities/constants/poisons.js
@@ -1,90 +1,112 @@
 module.exports = [
   {
     label: '2,4-dinitrophenol and its compounds including dinitrophenolate',
-    value: '2,4-dinitrophenol and its compounds including dinitrophenolate'
+    value: '2,4-dinitrophenol and its compounds including dinitrophenolate',
+    shortLabel: 'DNP'
   },
   {
     label: 'Aluminium phosphide',
-    value: 'Aluminium phosphide'
+    value: 'Aluminium phosphide',
+    shortLabel: 'Aluminium phosphide'
   },
   {
     label: 'Aluminium sulfide',
-    value: 'Aluminium sulfide'
+    value: 'Aluminium sulfide',
+    shortLabel: 'Aluminium sulfide'
   },
   {
     label: 'Arsenic and its compounds',
-    value: 'Arsenic and its compounds'
+    value: 'Arsenic and its compounds',
+    shortLabel: 'Arsenic'
   },
   {
     label: 'Barium salts',
-    value: 'Barium salts'
+    value: 'Barium salts',
+    shortLabel: 'Barium salts'
   },
   {
     label: 'Bromomethane',
-    value: 'Bromomethane'
+    value: 'Bromomethane',
+    shortLabel: 'Bromomethane'
   },
   {
     label: 'Calcium phosphide',
-    value: 'Calcium phosphide'
+    value: 'Calcium phosphide',
+    shortLabel: 'Calcium phosphide'
   },
   {
     label: 'Calcium sulfide',
-    value: 'Calcium sulfide'
+    value: 'Calcium sulfide',
+    shortLabel: 'Calcium sulfide'
   },
   {
     label: 'Chloropicrin',
-    value: 'Chloropicrin'
+    value: 'Chloropicrin',
+    shortLabel: 'Chloropicrin'
   },
   {
     label: 'Fluoroacetic acid and its salts; fluoracetamide',
-    value: 'Fluoroacetic acid and its salts; fluoracetamide'
+    value: 'Fluoroacetic acid and its salts; fluoracetamide',
+    shortLabel: 'Fluoroacetic acid'
   },
   {
     label: 'Hydrogen cyanide and metal cyanides',
-    value: 'Hydrogen cyanide and metal cyanides'
+    value: 'Hydrogen cyanide and metal cyanides',
+    shortLabel: 'Hydrogen cyanide'
   },
   {
     label: 'Lead acetates; compound of lead with acids from fixed oils',
-    value: 'Lead acetates; compound of lead with acids from fixed oils'
+    value: 'Lead acetates; compound of lead with acids from fixed oils',
+    shortLabel: 'Lead acetates'
   },
   {
     label: 'Magnesium phosphide',
-    value: 'Magnesium phosphide'
+    value: 'Magnesium phosphide',
+    shortLabel: 'Magnesium phosphide'
   },
   {
     label: 'Magnesium sulfide',
-    value: 'Magnesium sulfide'
+    value: 'Magnesium sulfide',
+    shortLabel: 'Magnesium sulfide'
   },
   {
     label: 'Mercury and its compounds',
-    value: 'Mercury and its compounds'
+    value: 'Mercury and its compounds',
+    shortLabel: 'Mercury'
   },
   {
     label: 'Oxalic acid above 10% (weight by weight)',
-    value: 'Oxalic acid above 10% (weight by weight)'
+    value: 'Oxalic acid above 10% (weight by weight)',
+    shortLabel: 'Oxalic acid'
   },
   {
     label: 'Phenols above 60% w/w and their compounds',
-    value: 'Phenols above 60% w/w and their compounds'
+    value: 'Phenols above 60% w/w and their compounds',
+    shortLabel: 'Phenols'
   },
   {
     label: 'Phosphorus yellow',
-    value: 'Phosphorus yellow'
+    value: 'Phosphorus yellow',
+    shortLabel: 'Phosphorus yellow'
   },
   {
     label: 'Sodium sulfide',
-    value: 'Sodium sulfide'
+    value: 'Sodium sulfide',
+    shortLabel: 'Sodium sulfide'
   },
   {
     label: 'Strychnine, its salts and quaternary compounds',
-    value: 'Strychnine, its salts and quaternary compounds'
+    value: 'Strychnine, its salts and quaternary compounds',
+    shortLabel: 'Strychnine'
   },
   {
     label: 'Thallium and its salts',
-    value: 'Thallium and its salts'
+    value: 'Thallium and its salts',
+    shortLabel: 'Thallium'
   },
   {
     label: 'Zinc phosphide',
-    value: 'Zinc phosphide'
+    value: 'Zinc phosphide',
+    shortLabel: 'Zinc phosphide'
   }
 ];
