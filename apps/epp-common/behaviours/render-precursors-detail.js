ad60053a3fe17b1e89d8ab61a97c31943cab00dd
Vivek Kumar <Vivek.Kumar1@digital.homeoffice.gov.uk>
2025-02-26 11:33:54 +0000

    EPP 160 Precursor details page (#73)
    * EPP-160 initial commit
    
    * EPP-160 added validation error messages
    
    * EPP-160 added validation messages
    
    * EPP-160 updated next route
    
    * EPP-160 unit tests for string formatter
    
    * EPP-160 updated test description
    
    * EPP-160 save home address
    
    * EPP-160 resolve merge conflicts
    
    * EPP-160 bahaviour unit tests
    
    * EPP-160 moved behaviour into common
    
    * EPP-160 added code comments
    
    * EPP-160 save address behaviour tests
    
    * EPP-160 updated route and removed temp hint
    
    * EPP-160 move helpers in the util dir
    
    * EPP-160 resolved merge conflicts
    
    * EPP-160 updated next route
    
    * EPP-160 tests for helpers
    
    * EPP-160 updated tests description
    
    * EPP-160 textarea with updated
    
    * EPP-160 resolved merge conflicts
    
    * EPP-160 updated poison short labels

---
+++ apps/epp-common/behaviours/render-precursors-detail.js
@@ -0,0 +1,40 @@
+const { getPrecursorsShortLabel } = require('../../../utilities/helpers');
+
+/**
+ * @param {string} fieldName - The field name used on the select precursor page
+ */
+module.exports = fieldName => superclass =>
+  class extends superclass {
+    getValues(req, res, next) {
+      const selectedPrecursor = req.sessionModel.get(fieldName);
+      const whyNeedPrecursorLabel = getPrecursorsShortLabel(
+        `Why do you need ${selectedPrecursor}`
+      );
+      const whereToStorePrecursorLegend = getPrecursorsShortLabel(
+        `Where will you store the ${selectedPrecursor}`
+      );
+      const whereToUsePrecursorLegend = getPrecursorsShortLabel(
+        `Where will you use the ${selectedPrecursor}`
+      );
+      const storePrecursorOtherAddressLabel = getPrecursorsShortLabel(
+        `Storage address for the ${selectedPrecursor}`
+      );
+      const usePrecursorOtherAddressLabel = getPrecursorsShortLabel(
+        `Usage address for the ${selectedPrecursor}`
+      );
+
+      const labelValuesMap = {
+        whyNeedPrecursorLabel: whyNeedPrecursorLabel,
+        whereToStorePrecursorLegend: whereToStorePrecursorLegend,
+        whereToUsePrecursorLegend: whereToUsePrecursorLegend,
+        storePrecursorOtherAddressLabel: storePrecursorOtherAddressLabel,
+        usePrecursorOtherAddressLabel: usePrecursorOtherAddressLabel
+      };
+
+      Object.entries(labelValuesMap).forEach(([key, value]) => {
+        req.sessionModel.set(key, value);
+      });
+
+      return super.getValues(req, res, next);
+    }
+  };
