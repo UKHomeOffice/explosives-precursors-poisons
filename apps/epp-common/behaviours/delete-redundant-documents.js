df3acef5208bb004f25da6b2673a64c12d409143
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2025-03-06 09:50:36 +0000

    EPP-243 behaviour unit tests

--- apps/epp-common/behaviours/delete-redundant-documents.js
+++ apps/epp-common/behaviours/delete-redundant-documents.js
@@ -1,3 +1,12 @@
+/**
+ * Responsible for removing uploaded documents
+ * from the session when the related or dependent form field changes from 'yes' to 'no'
+ *
+ * @param {string} fieldName - The name of the form field to check.
+ * @param {string[]} uploadDocs - An array of field names representing uploaded documents.
+ * @returns {Function} - Returns a new class extending the superclass.
+ */
+
 module.exports = (fieldName, uploadDocs) => superclass =>
   class extends superclass {
     saveValues(req, res, next) {
