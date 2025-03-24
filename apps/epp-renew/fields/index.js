982b9279f70310aec307c346bfa749a8645d0075
Vivek Kumar <Vivek.Kumar1@digital.homeoffice.gov.uk>
2025-03-24 12:16:39 +0000

    EPP-259 Standardize labels (#118)
    * EPP-259 updated labels and legend size
    
    * EPP-258 updated classes
    
    * EPP-259 use legend class

--- apps/epp-renew/fields/index.js
+++ apps/epp-renew/fields/index.js
@@ -2,7 +2,6 @@
 module.exports = {
   'new-renew-licence-number': {
     mixin: 'input-text',
-    className: ['govuk-input', 'govuk-!-width-two-thirds'],
     labelClassName: 'visuallyhidden',
     validate: ['required', 'notUrl']
   }
