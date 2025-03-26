981063dca5ec0b9b38846a8eb5eabd7a0b0791c5
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2025-03-26 14:05:54 +0000

    EPP-215 resolved merge conflicts

--- apps/epp-replace/fields/index.js
+++ apps/epp-replace/fields/index.js
@@ -2,7 +2,10 @@ const titles = require('../../../utilities/constants/titles');
 const dateComponent = require('hof').components.date;
 const poisonsList = require('../../../utilities/constants/poisons.js');
 const countersignatoryYears = require('../../../utilities/constants/countersignatory-years.js');
-const { isValidUkDrivingLicenceNumber, validInternationalPhoneNumber } = require('../../../utilities/helpers');
+const {
+  isValidUkDrivingLicenceNumber,
+  validInternationalPhoneNumber
+} = require('../../../utilities/helpers');
 const countries = require('../../../utilities/constants/countries');
 const policeForces = require('../../../utilities/constants/police-forces.js');
 
@@ -319,8 +322,7 @@ module.exports = {
     options: [
       {
         value: '',
-        label:
-          'fields.replace-police-force.options.none_selected'
+        label: 'fields.replace-police-force.options.none_selected'
       }
     ].concat(policeForces),
     validate: ['required']
@@ -331,62 +333,62 @@ module.exports = {
     labelClassName: 'govuk-label--m',
     validate: ['required', { type: 'maxlength', arguments: 250 }, 'notUrl']
   },
-    'replace-home-address-1': {
-        mixin: 'input-text',
-        labelClassName: 'govuk-label--m',
-        validate: [
-            'required',
-            { type: 'minlength', arguments: 2 },
-            { type: 'maxlength', arguments: 250 },
-            'notUrl'
-        ]
-    },
-    'replace-home-address-2': {
-        mixin: 'input-text',
-        labelClassName: 'govuk-label--m',
-        validate: [
-            { type: 'minlength', arguments: 2 },
-            { type: 'maxlength', arguments: 250 },
-            'notUrl'
-        ]
-    },
-    'replace-home-town-or-city': {
-        mixin: 'input-text',
-        labelClassName: 'govuk-label--m',
-        validate: [
-            'required',
-            { type: 'minlength', arguments: 2 },
-            { type: 'maxlength', arguments: 250 },
-            'notUrl'
-        ]
-    },
-    'replace-home-county': {
-        mixin: 'input-text',
-        labelClassName: 'govuk-label--m',
-        validate: [
-            { type: 'minlength', arguments: 2 },
-            { type: 'maxlength', arguments: 250 },
-            'notUrl'
-        ]
-    },
-    'replace-home-postcode': {
-        mixin: 'input-text',
-        labelClassName: 'govuk-label--m',
-        className: ['govuk-input', 'govuk-input--width-10'],
-        formatter: ['ukPostcode']
-    },
-    'replace-home-country': {
-        mixin: 'select',
-        validate: ['required'],
-        labelClassName: 'govuk-label--m',
-        className: ['typeahead'],
-        options: [
-            {
-                value: '',
-                label: 'fields.replace-home-country.options.none_selected'
-            }
-        ].concat(countries)
-    },
+  'replace-home-address-1': {
+    mixin: 'input-text',
+    labelClassName: 'govuk-label--m',
+    validate: [
+      'required',
+      { type: 'minlength', arguments: 2 },
+      { type: 'maxlength', arguments: 250 },
+      'notUrl'
+    ]
+  },
+  'replace-home-address-2': {
+    mixin: 'input-text',
+    labelClassName: 'govuk-label--m',
+    validate: [
+      { type: 'minlength', arguments: 2 },
+      { type: 'maxlength', arguments: 250 },
+      'notUrl'
+    ]
+  },
+  'replace-home-town-or-city': {
+    mixin: 'input-text',
+    labelClassName: 'govuk-label--m',
+    validate: [
+      'required',
+      { type: 'minlength', arguments: 2 },
+      { type: 'maxlength', arguments: 250 },
+      'notUrl'
+    ]
+  },
+  'replace-home-county': {
+    mixin: 'input-text',
+    labelClassName: 'govuk-label--m',
+    validate: [
+      { type: 'minlength', arguments: 2 },
+      { type: 'maxlength', arguments: 250 },
+      'notUrl'
+    ]
+  },
+  'replace-home-postcode': {
+    mixin: 'input-text',
+    labelClassName: 'govuk-label--m',
+    className: ['govuk-input', 'govuk-input--width-10'],
+    formatter: ['ukPostcode']
+  },
+  'replace-home-country': {
+    mixin: 'select',
+    validate: ['required'],
+    labelClassName: 'govuk-label--m',
+    className: ['typeahead'],
+    options: [
+      {
+        value: '',
+        label: 'fields.replace-home-country.options.none_selected'
+      }
+    ].concat(countries)
+  },
   'amend-declaration': {
     mixin: 'checkbox',
     validate: ['required']
