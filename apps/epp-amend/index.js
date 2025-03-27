5b41076c63e383de3550c38f2958e3f235a0c96b
Vivek Kumar <Vivek.Kumar1@digital.homeoffice.gov.uk>
2025-03-27 11:06:31 +0000

    EPP-181 application submitted page (#133)

--- apps/epp-amend/index.js
+++ apps/epp-amend/index.js
@@ -424,9 +424,12 @@ module.exports = {
       behaviours: [SummaryPageBehaviour, SendNotification],
       sections: require('./sections/summary-data-sections'),
       fields: ['amend-declaration'],
-      next: '/application-submitted',
+      next: '/amendment-submitted',
       locals: { captionHeading: 'Section 23 of 23' }
     },
-    '/application-submitted': {}
+    '/amendment-submitted': {
+      clearSession: true,
+      backLink: false
+    }
   }
 };
