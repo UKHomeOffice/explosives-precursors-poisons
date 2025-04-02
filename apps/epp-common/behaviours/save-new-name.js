70360939f2940d78ea521dc5fdfe56c972662413
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2025-04-02 16:17:10 +0100

    EPP-182 format date for notify template

--- apps/epp-common/behaviours/save-new-name.js
+++ apps/epp-common/behaviours/save-new-name.js
@@ -1,10 +1,19 @@
+const { getFormattedDate } = require('../../../utilities/helpers');
 module.exports = fields => superclass =>
   class extends superclass {
     successHandler(req, res, next) {
+      const dateFields = [
+        'amend-new-date-name-changed',
+        'replace-date-new-name-changed'
+      ];
       if (Array.isArray(fields) && fields.length > 0) {
         const fullName = [];
         fields.forEach(field => {
-          fullName.push(req.form.values[field]);
+          if (dateFields.includes(field) && req.form.values[field]) {
+            fullName.push(getFormattedDate(req.form.values[field]));
+          } else {
+            fullName.push(req.form.values[field]);
+          }
         });
         const otherName = fullName.filter(Boolean).join('\n');
         req.sessionModel.set('formattedNewName', otherName);
