956f7655509f278af2ba439df1346f27f8c4115d
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2025-04-07 16:02:16 +0100

    EPP-206 poison summary page

--- apps/epp-replace/index.js
+++ apps/epp-replace/index.js
@@ -15,6 +15,12 @@ const AfterDateOfBirth = require('../epp-common/behaviours/after-date-validator'
 const NavigateNoChanges = require('./behaviours/navigate-no-changes');
 
 const RenderPoisonDetails = require('../epp-common/behaviours/render-poison-detail');
+const AggregateSaveEditPrecursorPoison = require('../epp-common/behaviours/aggregator-save-update-precursors-poisons');
+const ParseSummaryPrecursorsPoisons = require('../epp-common/behaviours/parse-summary-precursors-poisons');
+const EditRouteStart = require('../epp-common/behaviours/edit-route-start');
+const EditRouteReturn = require('../epp-common/behaviours/edit-route-return');
+
+const CounterSignatoryNavigation = require('../epp-common/behaviours/counter-signatory-navigation');
 
 // TODO: Use DeleteRedundantDocuments behaviour similar to amend flow to
 // remove the uploaded files when dependent option changes
@@ -24,6 +30,7 @@ module.exports = {
   views: 'apps/epp-replace/views',
   translations: 'apps/epp-replace/translations',
   baseUrl: '/replace',
+  params: '/:action?/:id?/:edit?',
   behaviours: [JourneyValidator],
   steps: {
     '/replace-licence': {
@@ -335,11 +342,30 @@ module.exports = {
         'store-poison-other-address',
         'poison-use-other-address'
       ],
-      next: '/poisons-summary',
+      next: '/poison-summary',
       locals: { captionHeading: 'Section 20 of 26' }
     },
-    '/poisons-summary': {
-      next: '/countersignatory-details',
+    '/poison-summary': {
+      behaviours: [
+        CounterSignatoryNavigation('/poison-summary'),
+        AggregateSaveEditPrecursorPoison,
+        ParseSummaryPrecursorsPoisons,
+        EditRouteReturn
+      ],
+      aggregateTo: 'poisons-details-aggregate',
+      aggregateFrom: [
+        'display-poison-title',
+        'why-need-poison',
+        'how-much-poison',
+        'compound-or-salt',
+        'what-concentration-poison',
+        'where-to-store-poison',
+        'where-to-use-poison'
+      ],
+      titleField: ['poison-field'],
+      addStep: 'select-poisons',
+      addAnotherLinkText: 'poison',
+      next: '/confirm',
       locals: { captionHeading: 'Section 20 of 26' }
     },
     '/countersignatory-details': {
@@ -399,7 +425,7 @@ module.exports = {
     },
     '/confirm': {
       sections: require('./sections/summary-data-sections'),
-      behaviours: [SummaryPageBehaviour],
+      behaviours: [SummaryPageBehaviour, EditRouteStart],
       next: '/declaration'
     },
     '/declaration': {
