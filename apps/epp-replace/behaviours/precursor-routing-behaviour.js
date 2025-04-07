57a545010299b9981476312fb89ea38ac97d5f45
Mike Maratheftis <mike.maratheftis@digital.homeoffice.gov.uk>
2025-04-07 15:15:54 +0100

    EPP-234 amend behaviour

--- apps/epp-replace/behaviours/precursor-routing-behaviour.js
+++ apps/epp-replace/behaviours/precursor-routing-behaviour.js
@@ -2,7 +2,7 @@ module.exports = superclass =>
   class PrecursorRoutingBehaviour extends superclass {
     successHandler(req, res) {
       const noPrecursors = req.sessionModel.get('replace-no-poisons-precursors-options') === 'no';
-      const nameChanged = req.sessionModel.get('replace-name') === 'yes';
+      const nameChanged = req.sessionModel.get('replace-name-options') === 'yes';
       const addressChanged = req.sessionModel.get('replace-home-address-options') === 'yes';
 
       if (noPrecursors) {
@@ -11,6 +11,10 @@ module.exports = superclass =>
 
       if (nameChanged || addressChanged) {
         return res.redirect(`${req.baseUrl}/countersignatory-details`);
+      }
+
+      if (!noPrecursors && !nameChanged && !addressChanged) {
+        return res.redirect(`${req.baseUrl}/confirm`);
       }
 
       return res.redirect(`${req.baseUrl}/confirm`);
