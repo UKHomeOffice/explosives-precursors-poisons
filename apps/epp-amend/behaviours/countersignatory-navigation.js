48e7dd623c63c0f35f7387087d3d6c2330c29ab9
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2025-04-08 14:55:40 +0100

    fix amend counter signatory navigation

---
+++ apps/epp-amend/behaviours/countersignatory-navigation.js
@@ -0,0 +1,49 @@
+module.exports = currentRoute => superclass =>
+  class extends superclass {
+    saveValues(req, res, next) {
+      const amendNameOrAddress =
+        req.sessionModel.get('amend-name-options') === 'yes' ||
+        req.sessionModel.get('amend-home-address-options') === 'yes';
+
+      const noAmendNameOrAddress =
+        req.sessionModel.get('amend-name-options') !== 'yes' &&
+        req.sessionModel.get('amend-home-address-options') !== 'yes';
+
+      const redirectToCountersignatory = () =>
+        res.redirect('/amend/countersignatory-details');
+      const redirectNoPoisonsOrPrecursors = () =>
+        res.redirect('/amend/no-poisons-or-precursors');
+      const redirectNoDetailsAmend = () =>
+        res.redirect('/amend/no-details-amend');
+
+      if (
+        currentRoute === '/poisons' &&
+        req.form.values['amend-poisons-option'] === 'no'
+      ) {
+        if (amendNameOrAddress) {
+          return redirectToCountersignatory();
+        }
+        if (noAmendNameOrAddress) {
+          return redirectNoPoisonsOrPrecursors();
+        }
+      }
+
+      if (currentRoute === '/poison-summary' && amendNameOrAddress) {
+        return redirectToCountersignatory();
+      }
+
+      if (
+        currentRoute === '/no-poisons-or-precursors' &&
+        req.form.values['amend-no-poisons-precursors-options'] === 'yes'
+      ) {
+        if (amendNameOrAddress) {
+          return redirectToCountersignatory();
+        }
+        if (noAmendNameOrAddress) {
+          return redirectNoDetailsAmend();
+        }
+      }
+
+      return super.saveValues(req, res, next);
+    }
+  };
